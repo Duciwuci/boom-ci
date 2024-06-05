1607a2372b8c8a202b83d185a5b12c7ea664616e
maiv <950579746@mail.schwarz>
2024-06-05 13:59:21 +0200

    hinzufügen neuer methoden

--- main.go
+++ main.go
@@ -14,12 +14,25 @@ func main() {
 
 	// Sorting the given slice
 	faktoren := []int{345, 78}
-	
-	
+
 	// Finding the time
-	fmt.Println("Das Ergebnis ist: ", multipliziere(faktoren[0],faktoren[1]))
+	fmt.Println("Das Ergebnis ist: ", multipliziere(faktoren[0], faktoren[1]))
+
+	// Finding the time
+	fmt.Println("Das Ergebnis ist: ", multipliziere2(faktoren[0], faktoren[1]))
+
+	// Finding the time
+	fmt.Println("Das Ergebnis ist: ", multipliziere3(faktoren[0], faktoren[1]))
 
 }
 func multipliziere(faktor1, faktor2 int) int {
+	return faktor1 * faktor2
+}
+
+func multipliziere2(faktor1, faktor2 int) int {
+	return faktor1 * faktor2
+}
+
+func multipliziere3(faktor1, faktor2 int) int {
 	return faktor1 * faktor2
 }
